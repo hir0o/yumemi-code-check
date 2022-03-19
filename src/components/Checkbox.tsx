1e38e6cab6ae4c61832820effde6692970164a6d
hir0o <mk.9.q.kyu@gmail.com>
2022-03-19 20:25:37 +0900

    Checkboxコンポーネントの作成

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,93 @@
+import React, { memo, VFC } from 'react'
+import { styled } from '@linaria/react'
+import cn from 'classnames'
+
+type Props = {
+  title: string
+  name: string
+}
+
+const StyledLabel = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  .label__check {
+    width: 12px;
+    height: 12px;
+    border: 2px solid #c8c7c8;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    background: white;
+    &::before {
+      content: '';
+      display: inline-block;
+      transform: rotate(45deg);
+      margin-bottom: 3px;
+      height: 60%;
+      width: 30%;
+      border-bottom: 2px solid white;
+      border-right: 2px solid white;
+    }
+    &::after {
+      content: '';
+      display: inline-block;
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-color: white;
+    }
+    &--checked {
+      background-color: red;
+      &::after {
+        background-color: red;
+        left: 100%;
+        transition: 0.2s ease-in-out left;
+      }
+    }
+  }
+  .label__input {
+    border: 0px;
+    clip: rect(0px, 0px, 0px, 0px);
+    height: 1px;
+    width: 1px;
+    margin: -1px;
+    padding: 0px;
+    overflow: hidden;
+    white-space: nowrap;
+    position: absolute;
+  }
+`
+
+const Checkbox: VFC<Props> = ({ name, title }) => {
+  const [isChecked, setIsChecked] = React.useState(false)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target
+    setIsChecked(checked)
+  }
+
+  return (
+    <StyledLabel htmlFor={name}>
+      <input
+        className="label__input"
+        type="checkbox"
+        id={name}
+        name={name}
+        onChange={handleChange}
+      />
+      <span
+        className={cn('label__check', {
+          'label__check--checked': isChecked,
+        })}
+      />
+      <span className="label__text">{title}</span>
+    </StyledLabel>
+  )
+}
+
+export default memo(Checkbox)
